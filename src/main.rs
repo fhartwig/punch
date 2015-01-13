c966531366d8cb726bfba1895d4cb931641c8694
Florian Hartwig <florian.j.hartwig@gmail.com>
2015-01-13 21:48:20 +0100

    Add timeclock object to avoid some repetition, fix warnings

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,84 @@
-extern crate getopts;
+#![allow(unstable)]
+
 extern crate time;
 
 use std::os::{homedir, args};
-use std::io::{File, SeekStyle, FileMode, FileAccess, FilePermission, USER_RWX};
+use std::io::{File, SeekStyle, FileMode, FileAccess, USER_RWX};
 use std::io::fs::{unlink, PathExtensions, mkdir};
-use getopts::{reqopt, getopts, usage};
 use time::{now_utc, Tm};
 
 fn main() {
-    let now = now_utc();
     let args = args();
-    let home = homedir().unwrap();
-    let base_dir = home.join(Path::new(".punch"));
-    if !base_dir.exists() {
-        mkdir(&base_dir, USER_RWX);
-    }
-    let timesheet_path = base_dir.join("timesheet");
-    let mut working_state_path = base_dir.join("state");
-    
-    let mut timesheet = File::open_mode(&timesheet_path,
-                                        FileMode::Append,
-                                        FileAccess::Write).unwrap();
-    timesheet.seek(0, SeekStyle::SeekEnd);
-
     match args.get(1) {
         None => panic!("No command given"),
         Some(command) => {
+            let mut time_clock = TimeClock::new();
             match &command[] {
-                "in" =>
-                    punch_in(&now, &mut timesheet, &mut working_state_path),
-                "out" =>
-                    punch_out(&now, &mut timesheet, &mut working_state_path),
+                "in" => time_clock.punch_in(),
+                "out" => time_clock.punch_out(),
                 _ => panic!("unknown command")
             }
         }
     }
 }
 
-fn punch_in(now: &Tm, timesheet: &mut File, state: &Path) {
-    if currently_working(state) {
-        panic!("You're already working");
+struct TimeClock {
+    now: Tm,
+    timesheet: File,
+    state_path: Path // TODO: maybe we can make this a bool that we
+                           // initalise with the TimeClock
+                           // and write back when we destroy the timeclock
+                            // object
+}
+
+impl TimeClock {
+    fn new() -> TimeClock {
+        let now = now_utc();
+        let home = homedir().unwrap();
+        let base_dir = home.join(Path::new(".punch"));
+        let timesheet_path = base_dir.join("timesheet");
+        let working_state_path = base_dir.join("state");
+        if !base_dir.exists() {
+            mkdir(&base_dir, USER_RWX).unwrap();
+        }
+        let timesheet = File::open_mode(&timesheet_path,
+                                            FileMode::Append,
+                                            FileAccess::Write).unwrap();
+        TimeClock {
+            timesheet: timesheet,
+            state_path: working_state_path,
+            now: now
+        }
     }
-    writeln!(timesheet, "in: {}", now.rfc822());
-    set_current_working_state(true, state);
-}
 
-fn punch_out(now: &Tm, timesheet: &mut File, state: &Path) {
-    if !currently_working(state) {
-        panic!("Can't punch out if you're not working!");
+    fn punch_in(&mut self) {
+        if self.currently_working() {
+            panic!("You're already working");
+        }
+        self.timesheet.seek(0, SeekStyle::SeekEnd).unwrap();
+        writeln!(&mut self.timesheet, "in: {}", self.now.rfc822()).unwrap();
+        self.set_current_working_state(true);
     }
-    writeln!(timesheet, "out: {}", now.rfc822());
-    set_current_working_state(false, state);
-}
 
-fn currently_working(p: &Path) -> bool {
-    p.exists()
-}
+    fn punch_out(&mut self) {
+        if !self.currently_working() {
+            panic!("Can't punch out if you're not working!");
+        }
+        self.timesheet.seek(0, SeekStyle::SeekEnd).unwrap();
+        writeln!(&mut self.timesheet, "out: {}", self.now.rfc822()).unwrap();
+        self.set_current_working_state(false);
+    }
 
-fn set_current_working_state(currently_working: bool, p: &Path) {
-    if currently_working {
-        File::create(p);
-    } else {
-        unlink(p);
+
+    fn set_current_working_state(&self, currently_working: bool) {
+        if currently_working {
+            File::create(&self.state_path).unwrap();
+        } else {
+            unlink(&self.state_path).unwrap();
+        }
+    }
+
+    fn currently_working(&self) -> bool {
+        self.state_path.exists()
     }
 }
