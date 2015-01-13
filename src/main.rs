258e7e79ca8d8bb4a371049ca9b54c9432e6fd34
Florian Hartwig <florian.j.hartwig@gmail.com>
2015-01-13 21:07:48 +0100

    Track whether we're punched in or not, to make sure we don't put nonsense data in the time sheet

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,22 @@ extern crate getopts;
 extern crate time;
 
 use std::os::{homedir, args};
-use std::io::{File, SeekStyle, FileMode, FileAccess};
+use std::io::{File, SeekStyle, FileMode, FileAccess, FilePermission, USER_RWX};
+use std::io::fs::{unlink, PathExtensions, mkdir};
 use getopts::{reqopt, getopts, usage};
 use time::{now_utc, Tm};
 
 fn main() {
+    let now = now_utc();
     let args = args();
     let home = homedir().unwrap();
-    let timesheet_path = home.join(Path::new(".punch"));
-
-    let now = now_utc();
-
+    let base_dir = home.join(Path::new(".punch"));
+    if !base_dir.exists() {
+        mkdir(&base_dir, USER_RWX);
+    }
+    let timesheet_path = base_dir.join("timesheet");
+    let mut working_state_path = base_dir.join("state");
+    
     let mut timesheet = File::open_mode(&timesheet_path,
                                         FileMode::Append,
                                         FileAccess::Write).unwrap();
@@ -22,20 +27,40 @@ fn main() {
         None => panic!("No command given"),
         Some(command) => {
             match &command[] {
-                "in" => punch_in(&now, &mut timesheet),
-                "out" => punch_out(&now, &mut timesheet),
+                "in" =>
+                    punch_in(&now, &mut timesheet, &mut working_state_path),
+                "out" =>
+                    punch_out(&now, &mut timesheet, &mut working_state_path),
                 _ => panic!("unknown command")
             }
         }
     }
 }
 
-fn punch_in(now: &Tm, timesheet: &mut File) {
+fn punch_in(now: &Tm, timesheet: &mut File, state: &Path) {
+    if currently_working(state) {
+        panic!("You're already working");
+    }
     writeln!(timesheet, "in: {}", now.rfc822());
-    // TODO: make sure we're not already punched in
+    set_current_working_state(true, state);
 }
 
-fn punch_out(now: &Tm, timesheet: &mut File) {
+fn punch_out(now: &Tm, timesheet: &mut File, state: &Path) {
+    if !currently_working(state) {
+        panic!("Can't punch out if you're not working!");
+    }
     writeln!(timesheet, "out: {}", now.rfc822());
-    // TODO: make sure we're punched in
+    set_current_working_state(false, state);
+}
+
+fn currently_working(p: &Path) -> bool {
+    p.exists()
+}
+
+fn set_current_working_state(currently_working: bool, p: &Path) {
+    if currently_working {
+        File::create(p);
+    } else {
+        unlink(p);
+    }
 }
