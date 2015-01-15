f14a9e020ae004756d541f666afe7e5e8453cc20
Florian Hartwig <florian.j.hartwig@gmail.com>
2015-01-15 01:12:12 +0100

    Add report and status commands

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@
 extern crate time;
 
 use std::os::{homedir, args};
-use std::io::{File, SeekStyle, FileMode, FileAccess, USER_RWX};
+use std::io::{File, SeekStyle, FileMode, FileAccess, USER_RWX, BufferedReader};
 use std::io::fs::{unlink, PathExtensions, mkdir};
-use time::{now_utc, Tm};
+use time::{now_utc, Tm, empty_tm, strptime};
+use std::time::Duration;
+use std::io::BufferPrelude;
 
 fn main() {
     let args = args();
@@ -16,19 +18,21 @@ fn main() {
             match &command[] {
                 "in" => time_clock.punch_in(),
                 "out" => time_clock.punch_out(),
+                "status" => time_clock.status(),
+                "report" => time_clock.print_by_days(),
                 _ => panic!("unknown command")
             }
         }
     }
 }
 
+
+
 struct TimeClock {
     now: Tm,
     timesheet: File,
-    state_path: Path // TODO: maybe we can make this a bool that we
-                           // initalise with the TimeClock
-                           // and write back when we destroy the timeclock
-                            // object
+    currently_working: bool,
+    state_path: Path
 }
 
 impl TimeClock {
@@ -46,13 +50,14 @@ impl TimeClock {
                                             FileAccess::Write).unwrap();
         TimeClock {
             timesheet: timesheet,
+            currently_working: working_state_path.exists(),
             state_path: working_state_path,
             now: now
         }
     }
 
     fn punch_in(&mut self) {
-        if self.currently_working() {
+        if self.currently_working {
             panic!("You're already working");
         }
         self.timesheet.seek(0, SeekStyle::SeekEnd).unwrap();
@@ -61,7 +66,7 @@ impl TimeClock {
     }
 
     fn punch_out(&mut self) {
-        if !self.currently_working() {
+        if !self.currently_working {
             panic!("Can't punch out if you're not working!");
         }
         self.timesheet.seek(0, SeekStyle::SeekEnd).unwrap();
@@ -70,7 +75,8 @@ impl TimeClock {
     }
 
 
-    fn set_current_working_state(&self, currently_working: bool) {
+    fn set_current_working_state(&mut self, currently_working: bool) {
+        self.currently_working = currently_working;
         if currently_working {
             File::create(&self.state_path).unwrap();
         } else {
@@ -78,7 +84,61 @@ impl TimeClock {
         }
     }
 
-    fn currently_working(&self) -> bool {
-        self.state_path.exists()
+    fn print_by_days(&mut self) {
+        self.timesheet.seek(0, SeekStyle::SeekSet).unwrap();
+        let mut buf = BufferedReader::new(File::open(self.timesheet.path()));
+        let mut starting_time: Option<Tm> = None;
+        let mut current_day = empty_tm();
+        let mut time_worked_today = Duration::zero();
+        for l in buf.lines() {
+            // lines should alternate between starting with "in: " and "out: "
+            let line = l.unwrap();
+            if let Some(start) = starting_time {
+                if !line.starts_with("out: ") {
+                    panic!("Bad data in timesheet!");
+                }
+                let time_str = &line[5..];
+                let end_time = parse_time(time_str);
+                let time_passed = end_time.to_timespec() - start.to_timespec();
+                time_worked_today = time_worked_today + time_passed;
+                if !same_day(&current_day, &start) {
+                    if time_worked_today > Duration::days(1) {
+                        panic!("Worked more than 24 hours in a day!");
+                    }
+                    if !time_worked_today.is_zero() {
+                        println!("{}: {}:{}",
+                            start.strftime("%a, %d %b %Y").unwrap(),
+                            time_worked_today.num_hours() ,
+                            time_worked_today.num_minutes() % 60
+                        );
+                    }
+                    current_day = start;
+                }
+                starting_time = None;
+            } else {
+                if !line.starts_with("in: ") {
+                    panic!("Bad data in timesheet!");
+                }
+                let time_str = &line[4..];
+                let t = parse_time(time_str);
+                starting_time = Some(t);
+            }
+        }
     }
+
+    fn status(&self) {
+        if self.currently_working {
+            println!("You're punched in")
+        } else {
+            println!("You're punched out")
+        }
+    }
+}
+
+fn parse_time(s: &str) -> Tm {
+    strptime(s.slice_to(s.len() - 1), "%a, %d %b %Y %T %Z").unwrap()
+}
+
+fn same_day(t1: &Tm, t2: &Tm) -> bool {
+    t1.tm_year == t2.tm_year && t1.tm_yday == t2.tm_yday
 }
